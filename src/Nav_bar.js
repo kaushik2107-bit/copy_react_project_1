af5f11521933e7e2b096c0165ea4f559a7cd7497
Rutvik-R <rutvikranpariya1221@gmail.com>
2022-08-27 15:28:37 +0530

    no-idea

---
+++ src/Nav_bar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Nav_bar() {
+    return (
+        <div className="navbar">
+            <div className="logo"></div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
