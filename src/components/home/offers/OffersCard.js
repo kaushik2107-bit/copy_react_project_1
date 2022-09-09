371bd38d070bb3700ee6b83c3c33b557e8edf34b
kaushik2107-bit <kaushiksbhowmick@gmail.com>
2022-09-09 12:54:18 +0530

    reviews component completed

--- src/components/home/offers/OffersCard.js
+++ src/components/home/offers/OffersCard.js
@@ -1,14 +1,21 @@
 import React from "react"
 import "../../../css/offercCard.css"
 
+
 export default function OffersCard(props) {
     return (
-        <div className="offer" style={{backgroundImage: "url(" + props.data.image + ")"}}>
-            <div className="offer-tag">{props.data.discount}</div>
-            <p>{props.data.name}</p>
-            <p>{props.data.main_price}</p>
-            <p>{props.data.main_price - props.data.main_price * props.data.discount/100}</p>
-            <button>Order Now</button>
+        <div className="offer">
+            <div className="image" style={{backgroundImage: "url(" + props.data.image + ")"}}/>
+            <div className="content">
+                <div className="star" style={{backgroundImage: `url(images/offers/star-offers.webp)`}}>{props.data.discount}%</div>
+                <div className="day">
+                    OFF ON {props.data.day}
+                </div>
+                <div className="cococola"></div>
+                <div className="text">
+                    <p>Medium and Large size pizza ONLY</p>
+                </div>
+            </div>
         </div>
     )
 }
